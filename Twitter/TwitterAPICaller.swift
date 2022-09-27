9c0c49c09f9a8e6152d955d009b3f483b3dee2ef
vnesslange <langev@oregonstate.edu>
2022-09-27 00:03:21 -0400

    update

--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -10,7 +10,7 @@ import UIKit
 import BDBOAuth1Manager
 
 class TwitterAPICaller: BDBOAuth1SessionManager {    
-    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "5lUJuO5AUpPUCez4ewYDFrtgh", consumerSecret: "s5ynGqXzstUZwFPxVyMDkYh197qvHOcVM3kwv1o2TKhS1avCdS")
+    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "Ms2Fxn2LAQ6H1R5HuT3HxHF0O", consumerSecret: "12ckKenkm3eSDpyUQichYxcS5hfmmlz0WdbwSYfo3k71dZiIjs")
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
     
